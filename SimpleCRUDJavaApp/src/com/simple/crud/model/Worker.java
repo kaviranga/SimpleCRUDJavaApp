572ea04db5457cd8d556b793ecd70f7342418d73
Duminda Kaviranga Gunawardhana <kaviranga5955@gmail.com>
2020-03-31 08:49:41 +0530

    Added mutator methods for Worker.java
    Added getters and setters

--- SimpleCRUDJavaApp/src/com/simple/crud/model/Worker.java
+++ SimpleCRUDJavaApp/src/com/simple/crud/model/Worker.java
@@ -1,1 +1,45 @@
+package com.simple.crud.model;
 
+public class Worker{
+  
+    private Long workerId;
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+
+    public Worker(){
+      
+    }
+  
+    public void setWorkerId(Long workerId){
+        this.workerId = workerId;
+    }
+  
+    public void getWorkerId(){
+        return workerId;
+    }
+  
+    public void setfirstName(String firstName){
+        this.firstName = firstName;
+    }
+  
+    public void getfirstName(){
+        return firstName;
+    }
+  
+    public void setlastName(String lastName){
+        this.lastName = lastName;
+    }
+  
+    public void getlastName(){
+        return lastName;
+    }
+    
+    public void setemailAddress(String emailAddress){
+        this.emailAddress = emailAddress;
+    }
+  
+    public void getemailAddress(){
+        return emailAddress;
+    }
+}
