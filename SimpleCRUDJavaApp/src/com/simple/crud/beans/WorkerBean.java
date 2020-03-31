2a7a80be4c5f3fe9a5d745f7cea8a65de7b59a11
Duminda Kaviranga Gunawardhana <kaviranga5955@gmail.com>
2020-03-31 09:14:18 +0530

    Updated WorkerBean.java

--- SimpleCRUDJavaApp/src/com/simple/crud/beans/WorkerBean.java
+++ SimpleCRUDJavaApp/src/com/simple/crud/beans/WorkerBean.java
@@ -1,1 +1,17 @@
+package com.simple.crud.beans;
 
+import com.sun.rowset.JdbcRowSetImpl;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.sql.rowset.JdbcRowSet;
+
+import com.simple.crud.model.Worker;
+
+
+public class WorkerBean {
+  
+  public WorkerBean(){
+  }
+  
+}
