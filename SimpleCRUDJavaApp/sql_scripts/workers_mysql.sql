fb325b4e52f3f22c59c0a7d91c9987f0bb5f541f
Duminda Gunawardhana <kaviranga5955@gmail.com>
2020-12-02 17:47:19 +0530

    Removed unwanted SimpleCRUDJavaApp folder and contents

--- SimpleCRUDJavaApp/sql_scripts/workers_mysql.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE workers(
-workerid int(11) AUTO_INCREMENT,  
-firstname varchar(30),
-lastname varchar(30),
-emailaddress varchar(30));
--- SimpleCRUDJavaApp/sql_scripts/workers_postgres.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE workers(
-workerid SERIAL,  
-firstname varchar(30),
-lastname varchar(30),
-emailaddress varchar(30));
